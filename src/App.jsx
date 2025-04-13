bfaa55691a4eca9bcfe4bc5d00e4555bedf90921
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2025-04-13 17:16:12 +0200

    add svg animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import FeatureSection from "./components/FeatureSection";
 import FooterSection from "./components/FooterSection";
 import HeroSection from "./components/HeroSection";
@@ -5,6 +7,7 @@ import Navbar from "./components/Navbar";
 import PricingSection from "./components/PricingSection";
 import TestimonialSection from "./components/TestimonialSection";
 import WorkflowSection from "./components/WorkflowSection";
+import brainSymbol from "./brain.svg";
 import {
   checklistItems,
   communityLinks,
@@ -16,21 +19,59 @@ import {
 } from "./constants";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    const timer1 = setTimeout(() => {
+      setFadeOut(true); // Start fade out
+    }, 4000); // 4s animation done
+
+    const timer2 = setTimeout(() => {
+      setIsLoading(false); // Remove after fade
+      document.body.classList.remove("overflow-hidden");
+    }, 4500);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <div className="max-w-7xl pb-10 mx-auto px-6 flex flex-col items-center jucstify-center space-y-20">
-        <HeroSection />
-        <FeatureSection features={features} />
-        <WorkflowSection checklistItems={checklistItems} />
-        <PricingSection pricingOptions={pricingOptions} />
-        <TestimonialSection testimonials={testimonials} />
-        <FooterSection
-          resourcesLinks={resourcesLinks}
-          platformLinks={platformLinks}
-          communityLinks={communityLinks}
-        />
-      </div>
+      {isLoading && (
+        <div
+          className={`fixed top-0 left-0 w-screen h-screen z-[100] flex items-center justify-center bg-black transition-opacity duration-1000 ${
+            fadeOut ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          <img
+            src={brainSymbol}
+            className="w-[300px] h-auto animate-fade-in"
+            alt="Splash Logo"
+          />
+        </div>
+      )}
+
+      <>
+        <Navbar />
+        <div className="max-w-7xl pb-10 mx-auto px-6 flex flex-col items-center jucstify-center space-y-20">
+          <HeroSection />
+          <FeatureSection features={features} />
+          <WorkflowSection checklistItems={checklistItems} />
+          <PricingSection pricingOptions={pricingOptions} />
+          <TestimonialSection testimonials={testimonials} />
+          <FooterSection
+            resourcesLinks={resourcesLinks}
+            platformLinks={platformLinks}
+            communityLinks={communityLinks}
+          />
+        </div>
+      </>
     </>
   );
 }
